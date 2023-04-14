a35a62466768c18aa7aaf8e8567ff7acb77534ca
Bibhabasu <bibhabasucvsc@kgpian.iitkgp.ac.in>
2023-04-14 15:50:14 +0530

    Responsiveness donFirst commit

---
+++ src/Data/QuizData.js
@@ -0,0 +1,22 @@
+export const QuizData = [
+    {
+        question: "What is the capital of India?",
+        options:[ "Delhi","Agra","Bombay","Madras"],
+        answer: 1
+    },
+    {
+        question: "Which one is the smallest ocean in the World?",
+        options:["Indian", "Pacific", "Atlantic", "Arctic"],
+        answer: 4
+    },
+    {
+        question: " Which country is also known as the Land of Rising Sun?",
+        options: ["Japan", "New Zealand", "Fiji", "China"],
+        answer: 1
+    },
+    {
+        question: "Which continent has the highest number of countries?",
+        options: ["Asia", "Europe", "North America", "Africa"],
+        answer:4
+    },
+];
